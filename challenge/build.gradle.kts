8af1d7aacefdc131144e964a6d90cb12a94a240c
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2026-03-07 18:47:47 +0300

    feature : add a project
    * Added a project

---
+++ challenge/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("java")
+    id("org.springframework.boot") version "4.0.3"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.team"
+version = "0.0.1-SNAPSHOT"
+description = "challenge"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation(kotlin("script-runtime"))
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
