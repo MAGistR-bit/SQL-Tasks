8b064d9011bc4f92d10e49235000d7fbf6456b68
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2026-03-08 15:46:07 +0300

    feature : add a sonar qube to the project
    * Added a SonarQube

--- challenge/build.gradle.kts
+++ challenge/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("org.springframework.boot") version "4.0.3"
     id("io.spring.dependency-management") version "1.1.7"
     id("io.freefair.lombok") version "9.2.0"
+    id("org.sonarqube") version "7.2.3.7755"
 }
 
 group = "com.team"
@@ -26,6 +27,13 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation(kotlin("script-runtime"))
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "MAGistR-bit_SQL-Tasks")
+        property("sonar.organization", "mikhail-organization")
+    }
 }
 
 tasks.withType<Test> {
